3dae804a0d3b844b9e63aba849b7aa508fcf2613
jarrett <jarrett@uchicago.edu>
2009-05-16 22:39:48 -0500

    Added the beginnings of the Cleaner spec.

--- spec/cleaner_spec.rb
+++ spec/cleaner_spec.rb
@@ -1,5 +1,49 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/rbbcode')
 
 describe RbbCode::Cleaner do
+	def make_tokens(tuples)
+		tuples.collect do |tuple|
+			token = RbbCode::Token.new(tuple[0])
+			token.text = tuple[1]
+			token
+		end
+	end
 	
+	it 'should remove unmatched opening tags' do
+		input = make_tokens([
+			[:text, 'This '],
+			[:opening_tag, '[b]'],
+			[:text, 'is unmatched, but this '],
+			[:opening_tag, '[i]'],
+			[:text, 'is not'],
+			[:closing_tag, '[/i]']
+		])
+		
+		expected = make_tokens([
+			[:text, 'This '],
+			[:text, 'is unmatched, but this '],
+			[:opening_tag, '[i]'],
+			[:text, 'is not'],
+			[:closing_tag, '[/i]']
+		])
+		
+		cleaned = RbbCode::Cleaner.new(@tokens).clean
+		cleaned.should == expected
+	end
+	
+	it 'should remove unmatched closing tags' do
+		input = make_tokens([
+			[:text, 'This '],
+			[:closing_tag, '[/i]'],
+			[:text, ' tag is unmatched']
+		])
+		
+		expected = make_tokens([
+			[:text, 'This '],
+			[:text, ' tag is unmatched']
+		])
+		
+		cleaned = RbbCode::Cleaner.new(@tokens).clean
+		cleaned.should == expected
+	end
 end
