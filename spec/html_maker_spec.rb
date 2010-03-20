7710b4a168c0f60fa952b96849243104e0b61d06
jarrett <jarrett@uchicago.edu>
2010-03-20 00:14:58 -0500

    Added # encoding: UTF-8 to the specs

--- spec/html_maker_spec.rb
+++ spec/html_maker_spec.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require File.expand_path(File.dirname(__FILE__) + '/node_spec_helper')
 
--- spec/node_spec_helper.rb
+++ spec/node_spec_helper.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 module RbbCode
 	class RootNode
 		def == (other_node)
